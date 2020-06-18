dd70e9804a8178ffc3cfd17372e9c8cf3cd53956
seohwa Jeong <suhaw0325@naver.com>
2020-06-18 10:06:39 +0900

    first commit

---
+++ upload/Upload_linux/src/main/java/com/yb/spring/files/upload/SpringBootUploadFilesApplication.java
@@ -0,0 +1,35 @@
+package com.yb.spring.files.upload;
+
+import javax.annotation.Resource;
+
+import com.yb.spring.files.upload.service.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+
+@EnableEurekaClient
+
+public class SpringBootUploadFilesApplication implements CommandLineRunner {
+  @Resource
+  FilesStorageService storageService;
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootUploadFilesApplication.class, args);
+
+  }
+
+
+  @Override
+  public void run(String... arg) throws Exception {
+//    storageService.deleteAll();
+//    storageService.init();
+  }
+
+}
+
